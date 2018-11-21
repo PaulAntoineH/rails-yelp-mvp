258393737646806fd699337daf3445acb70aef56
PaulAntoine Huilizen <paul.antoine.huilizen@gmail.com>
2018-11-21 16:30:57 +0100

    second exercise

---
+++ test/controllers/model_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class ModelControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get model_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get model_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get model_new_url
+    assert_response :success
+  end
+
+end
